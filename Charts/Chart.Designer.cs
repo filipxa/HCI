463604c5bdb3eccb5149e5c1de7af0896762c222
zrnkemiki <zrnkemiki@gmail.com>
2018-04-15 22:20:11 +0200

    gui quick_fix

--- Charts/Chart.Designer.cs
+++ Charts/Chart.Designer.cs
@@ -127,12 +127,10 @@
             this.Controls.Add(this.groupBoxGraphControl);
             this.Controls.Add(this.cartesianChart1);
             this.Name = "Chart";
-<<<<<<< HEAD
+
             this.Text = "Charts";
-=======
-            this.Text = "Form1";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Chart_FormClosed);
->>>>>>> master
+
             this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBoxGraphControl.ResumeLayout(false);
             this.groupBoxGraphControl.PerformLayout();
